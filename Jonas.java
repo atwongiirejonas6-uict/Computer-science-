cf5b769245b543463c4bf2ee20c769719356cca5
atwongiirejonas6-uict <atwongiirejonas6@gmail.com>
2026-09-18 16:26:01 +0300

    Add files via upload

---
+++ Jonas.java
@@ -0,0 +1,11 @@
+public class {
+    public static void main(String[] args){
+        double num1=10;
+        double num2=20;
+        double num3=30;
+        double num4=40;
+        double num5=50;
+        double average =(num1+num2+num3+num4+num5)/5;
+        System.out.println("Average =  :"+average);
+    }
+}

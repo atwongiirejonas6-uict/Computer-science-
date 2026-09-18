bf11da4a5b72bba040daf7fbaa4769187dee57a9
atwongiirejonas6-uict <atwongiirejonas6@gmail.com>
2026-09-18 16:26:56 +0300

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,16 @@
+public class Numbers {
+    public static void main(String[] args){
+        int num1= 10;
+        int num2= 20;
+        int sum = num1+num2;
+        System.out.println("Sum:"+sum);
+        int difference = num2-num1; 
+         System.out.println("Difference:"+difference);
+         if (num1!=0){int quotient= num2/num1;
+            System.out.println("Quotient:"+quotient);
+         }
+         else{
+               System.out.println("this can not be divided.");
+              }
+    }
+}
